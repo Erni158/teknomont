a8341aa51f21261e93ec09b8542961a7d43d6508
Ernest <ernest-kies@wp.pl>
2026-08-23 18:04:57 +0200

    fix

--- src/app/api/inquiry/route.ts
+++ src/app/api/inquiry/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
+//import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = "tmp"; // new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   try {
