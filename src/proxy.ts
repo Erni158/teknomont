38006a3a1fb7e14f3a47100a1a56202d62672b2e
Ernest <ernest-kies@wp.pl>
2026-08-29 14:40:06 +0200

    fix proxyt

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,22 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
+import createMiddleware from "next-intl/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
-export default createMiddleware(routing);
+import { routing } from "./i18n/routing";
+
+const intlMiddleware = createMiddleware(routing);
+
+export default function proxy(request: NextRequest) {
+  if (request.nextUrl.pathname === "/") {
+    const url = request.nextUrl.clone();
+
+    url.pathname = "/pl";
+
+    return NextResponse.redirect(url, 308);
+  }
+
+  return intlMiddleware(request);
+}
 
 export const config = {
-  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
